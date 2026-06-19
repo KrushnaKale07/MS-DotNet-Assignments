9168bc6873abcb07d88ad7a41cec91bc037a21f0
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-20 01:10:52 +0530

    added Abstract demo

---
+++ Day3_Work_Space/AbstractDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace AbstractDemo
+{
+    abstract class Person { 
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        abstract public void PrintData();
+    }
+
+    class Employee : Person { 
+        public int Id { get; set; }
+        public float Salary { get; set; }
+        public string Deptname { get; set; }
+
+        public override void PrintData()
+        {
+
+            Console.WriteLine("Id : " + this.Id + " Salary : " + this.Salary + " DeptName : "+ this.Deptname +" Name : " + this.Name+ " Age : "+this.Age);
+        }
+    }
+
+    class Student : Person { 
+        public int RollNo { get; set; }
+        public int Marks { get; set; }
+
+        public override void PrintData() {
+            Console.WriteLine("Roll No : "+this.RollNo+" Name : "+ this.Name+" Age : " + this.Age+ " Marks : " + this.Marks);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee() { Name = "Krushna", Age = 26, Deptname= " Mech", Salary=50000.00f, Id=101};
+            emp.PrintData();
+            Student s1 = new Student();
+        }
+    }
+}
