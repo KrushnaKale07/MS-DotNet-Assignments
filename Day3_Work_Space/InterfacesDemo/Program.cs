43d7f7a3ffe527e3bf58fd8f458295c6d62ddfe8
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-20 01:26:22 +0530

    added Interfaces demo

---
+++ Day3_Work_Space/InterfacesDemo/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace InterfacesDemo
+{
+
+    interface IMyInterface {
+        void Show();
+    }
+
+    class Test : IMyInterface {
+        public void Show() {
+            Console.WriteLine("Hello From Test");
+        }
+    }
+
+    interface IShape {
+        public void CalculateArea();
+        public void PrintArea();
+        public void PrintPerimeter();
+    }
+
+    class Shape : IShape { 
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
+
+        public void CalculateArea() { 
+        
+        }
+
+        public void PrintArea() {
+            Console.WriteLine("Area " + this.Area);
+        }
+        public void PrintPerimeter() { 
+            Console.WriteLine("Perimeter: "+this.Perimeter);
+        }
+    }
+
+    class Circle : Shape { 
+        public double Radius { get; set; }
+        public void CalculateArea() {
+            this.Area = Math.PI * this.Radius * this.Radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle();
+            c1.Radius = 23;
+            c1.CalculateArea();
+            c1.PrintArea();
+        }
+    }
+}
