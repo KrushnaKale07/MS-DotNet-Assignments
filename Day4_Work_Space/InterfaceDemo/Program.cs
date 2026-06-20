f6010910a74c0f822ab2e302c272eb5e80a77307
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-20 23:35:54 +0530

    added interface and abstract demos

---
+++ Day4_Work_Space/InterfaceDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace InterfaceDemo
+{
+
+    interface IShape {
+        void CalculateArea();
+        void CalculatePeri() {
+            Console.WriteLine("Default Method");
+        }
+    }
+
+    abstract class Shape { 
+        public double Area { get; set; }
+        public void PrintArea() { 
+            Console.WriteLine("Ares : " + this.Area);
+        }
+
+        public abstract void CalArea();
+    }
+
+    class Circle : IShape {
+        public void CalculateArea() { 
+        
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
