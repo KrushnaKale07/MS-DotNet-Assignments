13c352f498e1ef0a8ffe995fbba14bac3ff2df4c
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-20 21:42:48 +0530

    added types to display output on console

---
+++ Day4_Work_Space/TestOne/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TestOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number1 = 10;
+            string Name = "Malkeet";
+
+            Console.WriteLine("Number : "+Number1+" Name : "+Name);
+            Console.WriteLine("Number:{0}, Name {1}", Number1, Name);
+            Console.WriteLine($"Number : {Number1}, Name : {Name}");
+        }
+    }
+}
+namespace TestTwo
+{ }
