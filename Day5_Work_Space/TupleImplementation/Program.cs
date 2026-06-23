adcafc606591ae9b47d6d75cf312f32f959fe0a8
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-23 09:59:24 +0530

    added tuples

---
+++ Day5_Work_Space/TupleImplementation/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace TupleImplementation
+{
+    class Student { 
+        
+    }
+    class Teacher { 
+    
+    }
+    internal class Program
+    {
+        static (List<Student>, List<Teacher>) GetData()
+        {
+           List<Student> students = new List<Student>();
+            List<Teacher> teachers = new List<Teacher>();
+
+            return (students, teachers);
+        }
+        static (int, string, int) GetInfo() { 
+            int id = 1;
+            string name = "Kunal";
+            int age = 25;
+
+            return (id, name, age);
+        }
+        static void Main(string[] args)
+        {
+            Tuple<int, string, int> tuple = new Tuple<int, string, int>(1, "Krushna", 26);
+            Console.WriteLine("Id: "+ tuple.Item1+" Name: "+ tuple.Item2 + " Age: "+ tuple.Item3);
+
+            var info = Program.GetInfo();
+            Console.WriteLine(info);
+
+            var myData = Program.GetData();
+        }
+    }
+}
