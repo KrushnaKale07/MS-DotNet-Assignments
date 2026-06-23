ba00118dc2cdbc41aaa00df4b41c33606f7dc422
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-23 11:39:28 +0530

    added exception handling logic

---
+++ Day5_Work_Space/ExceptionHandling/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ExceptionHandling
+{
+
+
+    public class InvalidAgeException : Exception { 
+        public InvalidAgeException(string str) : base(str) { }
+    }
+    internal class Program
+    {
+
+        public static void CheckAge(int Age) {
+            if (Age < 18) {
+                throw new ArgumentException("Age is less then 18");
+            }
+            Console.WriteLine("You can vote");
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Program.CheckAge(19);
+            }
+            catch (InvalidAgeException e) {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine("Rest of my program");
+        }
+
+            static void Main1(string[] args)
+        {
+            int num1 = 100;
+            int num2 = 0;
+            float res = 0.0f;
+
+            try
+            {
+                res = num1 / num2;
+                Console.WriteLine("Res: " + res);
+            }
+            catch (DivideByZeroException ex) { 
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Rest of my program");
+        }
+    }
+}
