802fdfac29459155f5a9942d8602475c8af2d3ad
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-23 22:43:16 +0530

    added event handling using delegates

---
+++ Day6_Work_Space/EventHandlingUsingDelegate/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace EventHandlingUsingDelegate
+{
+    public class EventHandling {
+        public delegate void ScoreHandler(int score);
+
+        public static event ScoreHandler scoreReached;
+
+        static void Main(string[] args)
+        {
+            scoreReached += onScoreReached;
+
+            int score = 0;
+
+            for(int i = 0; i < 60; i++){
+                score = i;
+
+                Console.WriteLine("Courrent Score "+score);
+                Thread.Sleep(1000);
+                if (score>=50) {
+
+                    //Raise the Event which is defined as ref variable of delegate
+                    scoreReached?.Invoke(score);
+                    break;
+                }
+            }
+        }
+
+        static void onScoreReached(int score) {
+            Console.WriteLine("Event Triggered the score is: " + score);
+        }
+
+    }
+    //internal class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello, World!");
+    //    }
+    //}
+}
