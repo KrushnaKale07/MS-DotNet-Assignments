a721b6958ccd76fced3b21df42d85e21ebf66a50
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-24 11:13:41 +0530

    added new thread and start it using ThreadStart, Thread.Start() method

---
+++ Day6_Work_Space/MultiThreadingImplementation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MultiThreadingImplementation
+{
+    internal class Program
+    {
+        static void PrintMessage() { 
+            Thread.Sleep(1000);
+            Console.WriteLine("Hello From New Thread: " +Thread.CurrentThread.Name);
+            Thread.Sleep(1000);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, From Main: " +Thread.CurrentThread.Name);
+
+            ThreadStart ts = new ThreadStart(PrintMessage);
+
+            Thread th = new Thread(ts);
+
+            th.Name = "Thread- 1";
+
+            th.Start();
+
+            Console.WriteLine("Main is Executing: " +Thread.CurrentThread.Name);
+
+            Console.WriteLine("Main Ends Here: " + Thread.CurrentThread.Name);
+        }
+    }
+}
