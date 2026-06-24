1f608a51dc2dd7aefa8a9b84532901e97bc57948
KrushnaKale07 <krushnakale0007@gmail.com>
2026-06-24 13:21:02 +0530

    added ThreadSynchronization logoc

---
+++ Day6_Work_Space/ThreadSynchronization/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ThreadSynchronization
+{
+    public class Test { 
+        public void GenTable(Object obj) { 
+            int num1 =(int)obj;
+
+            for (int i = 1; i <= 10; i++) { 
+                Console.WriteLine($"{num1} * {i} = {num1*i}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            Thread thread1 = new Thread(new ParameterizedThreadStart(test.GenTable));
+            Thread thread2 = new Thread(new ParameterizedThreadStart(test.GenTable));
+            
+            thread1.Start(10);
+            thread2.Start(20);
+                
+        }
+
+    }
+}
